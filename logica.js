ca9a49ac5bdd91b8605f6c5db8f8aaf90704eb39
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-03-31 20:07:21 -0300

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,23 @@
+const url = 'https://ipapi.co/json/';
+
+// Faz uma requisição GET para o serviço de geolocalização de IP
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // Extrai as informações de localização do objeto retornado
+    const city = data.city;
+    const region = data.region;
+    const country = data.country_name;
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+
+    // Imprime as informações de localização no console
+    console.log(`Localização: ${city}, ${region}, ${country}`);
+    console.log(`Latitude: ${latitude}`);
+    console.log(`Longitude: ${longitude}`);
+
+    document.getElementById('latitude').innerHTML += latitude
+    document.getElementById('longitude').innerHTML += longitude
+  })
+  .catch(error => console.error(error));
+  
